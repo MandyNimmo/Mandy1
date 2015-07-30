215efaa2538f06c35bd132f27d3572294313a696
cs112mm <mandy.nimmo@digital.landregistry.gov.uk>
2015-07-30 11:15:39 +0100

    setup new repository

---
+++ acceptance-tests/features/support/config.rb
@@ -0,0 +1,14 @@
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
+
+include Capybara::DSL
+Capybara.default_selector = :xpath
+Capybara.default_wait_time = 10
+Capybara.app_host = 'http://localhost:4567'
+
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+ Capybara::Poltergeist::Driver.new(app, inspector: true, js_errors: true)
+end
